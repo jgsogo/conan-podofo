4af5b866b701010213901bbb8e72960d9e54f4c3
Javier G. Sogo <jgsogo@gmail.com>
2018-10-08 20:29:34 +0200

    list bin folder

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -22,4 +22,5 @@ class PodofoTestConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
+            self.run("ls -la")
             self.run(".%sexample" % os.sep)
