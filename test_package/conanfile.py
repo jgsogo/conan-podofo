5fa1a3b038c8d95926729751733f53272f2c7cba
Javier G. Sogo <jgsogo@gmail.com>
2018-10-08 20:40:27 +0200

    add LD_LIBRARY_PATH

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,6 +1,7 @@
+
 import os
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake, tools, RunEnvironment
 
 
 class PodofoTestConan(ConanFile):
@@ -21,6 +22,11 @@ class PodofoTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self.settings):
-            os.chdir("bin")
-            self.run("ls -la")
-            self.run(".%sexample" % os.sep)
+            with tools.environment_append(RunEnvironment(self).vars):
+                bin_path = os.path.join("bin", "example")
+                if self.settings.os == "Windows":
+                    self.run(bin_path)
+                elif self.settings.os == "Macos":
+                    self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+                else:
+                    self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
