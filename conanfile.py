3c8baf3d5a85c9091760a4dc6ffa2b30f3c1f172
Javier G. Sogo <jgsogo@gmail.com>
2018-10-08 14:21:39 +0200

    more consistent cmake flow

--- conanfile.py
+++ conanfile.py
@@ -14,27 +14,28 @@ class PodofoConan(ConanFile):
     exports_sources = ["CMakeLists.txt"]
 
     def requirements(self):
-        #self.requires("freetype/2.9.0@bincrafters/stable")
         self.requires("fontconfig/2.13.1@jgsogo/stable")
 
     def source(self):
         url = "http://sourceforge.net/projects/podofo/files/podofo/{version}/podofo-{version}.tar.gz"
         tools.get(url.format(version=self.version))
 
-    def build(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["PODOFO_BUILD_LIB_ONLY"] = True
-        cmake.definitions["SOURCE_FOLDER"] = "{name}-{version}".format(name=self.name, version=self.version)
-        cmake.configure(defs={'PODOFO_BUILD_SHARED:BOOL': 'TRUE',
-                                     'PODOFO_BUILD_STATIC:BOOL': 'FALSE',})
+        cmake.definitions["SOURCE_FOLDER"] = "{name}-{version}".format(name=self.name,
+                                                                       version=self.version)
+        cmake.configure(defs={'PODOFO_BUILD_SHARED:BOOL': 'TRUE' if self.options.shared else 'FALSE',
+                              'PODOFO_BUILD_STATIC:BOOL': 'FALSE' if self.options.shared else 'TRUE',
+                              })
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        cmake = CMake(self)
-        cmake.definitions["PODOFO_BUILD_LIB_ONLY"] = True
-        cmake.definitions["SOURCE_FOLDER"] = "{name}-{version}".format(name=self.name, version=self.version)
-        cmake.configure(defs={'PODOFO_BUILD_SHARED:BOOL': 'TRUE',
-                                     'PODOFO_BUILD_STATIC:BOOL': 'FALSE',})
+        cmake = self._configure_cmake()
         cmake.install()
 
     def package_info(self):
